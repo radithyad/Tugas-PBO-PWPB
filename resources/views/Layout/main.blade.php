a04a95e2e0421ee5f9679422a1c71991e30ae9fe
radithyad <radithya.dhiyaurrahman@gmail.com>
2022-12-04 17:39:54 +0700

    first commit

---
+++ resources/views/Layout/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+    crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+  <div class="wrapper d-flex align-items-stretch">
+			@include('Layout.sidebar')
+      @yield('content')
+		</div>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" 
+    crossorigin="anonymous"></script>
+  </body>
+</html>
+
---
+++ resources/views/Layout/sidebar.blade.php
@@ -0,0 +1,34 @@
+<nav id="sidebar" class="active">
+	<div class="custom-menu">
+		<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	        <i class="fa fa-bars"></i>
+	        <span class="sr-only">Toggle Menu</span>
+	    </button>
+    </div>
+	<div class="p-4">
+		<h1><a href="index.html" class="logo">Sidebar</a></h1>
+	    <ul class="list-unstyled components mb-5">
+	    	<li class="active">
+	            <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+	        </li>
+	        <li>
+	            <a href="#"><span class="fa fa-user mr-3"></span> Karyawan</a>
+	        </li>
+	        <li>
+              	<a href="#"><span class="fa fa-briefcase mr-3"></span> Jabatan</a>
+	        </li>
+	        <li>
+              	<a href="#"><span class="fa fa-sticky-note mr-3"></span> Gaji</a>
+	        </li>
+	        <li>
+              	<a href="#"><span class="fa fa-paper-plane mr-3"></span> Laporan</a>
+	        </li>
+	    </ul>            
+	<div class="footer">
+	    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+		Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+		<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+	</div>
+	</div>
+</nav>
+
